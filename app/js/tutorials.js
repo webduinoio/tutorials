73874cdd856fe22ea8dc14bb2c511c221648f55b
oxxo <oxxo.studio@gmail.com>
2015-12-06 23:33:06 +0800

    add next and pre arrow button

--- app/js/tutorials.js
+++ app/js/tutorials.js
@@ -1,6 +1,7 @@
 $(function(){
 	var file = location.href.split('/').pop(),
 			$window = $(window),
+			$document = $(document),
 			$pre = $('pre'),
 			$youtubeIframe = $('.youtube'),
 			$tutorials = $('.tutorials-content'),
@@ -9,12 +10,26 @@ $(function(){
 
 	$('a.tutorials').addClass('actived');
 
-	$tutorials.append('<div class="pre-next"><div>'+
+	$tutorials.append(
+			'<a class="arrow arrow-r"></a>'+
+			'<a class="arrow arrow-l"></a>'+
+			'<div class="pre-next"><div>'+
 			'<div class="pre"></div>'+
 			'<div class="next"></div>'+
 		'</div></div>'+
 		'<div class="others">'+
 		'</div>');
+
+	var $arrow = $('.arrow'),
+			$ar = $('.arrow-r'),
+			$al = $('.arrow-l');
+
+	if($window.width()>800){
+		_arrow_appear();
+		$window.scroll(_arrow_appear);
+	}else{
+		$arrow.hide();
+	}
 
 	$.getJSON('../json/tutorials.json',function(data){
 		var array = $.map(data, function(value, index) {
@@ -27,12 +42,18 @@ $(function(){
 				if(i>0 && i<(l-1)){
 					$('.pre').append('<i class="fa fa-hand-o-left"></i> 上一篇<br/><a href="'+array[i-1].src+'">'+array[i-1].title+'</a>');
 					$('.next').append('下一篇 <i class="fa fa-hand-o-right"></i><br/><a href="'+array[i+1].src+'">'+array[i+1].title+'</a>');
+					$ar.attr('href',array[i+1].src).attr('title','下一篇：'+array[i+1].title);
+					$al.attr('href',array[i-1].src).attr('title','上一篇：'+array[i-1].title);
 				}
 				else if(i==0){
 					$('.next').append('下一篇 <i class="fa fa-hand-o-right"></i><br/><a href="'+array[1].src+'">'+array[1].title+'</a>');
+					$al.css({'left':'-100px'});
+					$ar.attr('href',array[1].src).attr('title','下一篇：'+array[i+1].title);
 				}
 				else{
 					$('.pre').append('<i class="fa fa-hand-o-left"></i> 上一篇<br/><a href="'+array[i-1].src+'">'+array[i-1].title+'</a>');
+					$al.attr('href',array[i-1].src).attr('title','上一篇：'+array[i-1].title);
+					$ar.css({'right':'-100px'});
 				}
 			}
 		}
@@ -46,13 +67,26 @@ $(function(){
 	$youtubeIframe.css({'height':(youtubeWidth*9/16)+'px'});
 
 	$window.resize(function(){
-			youtubeWidth = $youtubeIframe.width();
-			$youtubeIframe.css({'height':(youtubeWidth*9/16)+'px'});
+		youtubeWidth = $youtubeIframe.width();
+		$youtubeIframe.css({'height':(youtubeWidth*9/16)+'px'});
+		if($window.width()>800){
+			_arrow_appear();
+		}else{
+			$arrow.hide();
+		}
 	});
 
 	$('.tutorials-content a').on('click',function(){
 		var linkurl = $(this).attr('href');
 		ga('send', 'event', 'tutorials', 'a click', linkurl, 4);
 	});
+
+	function _arrow_appear(){
+		if($window.scrollTop() > $banner.height() && $window.scrollTop() < ($document.height()-$window.height()-600)){
+			$arrow.show();
+		}else{
+			$arrow.hide();
+		}
+	}
 
 });
