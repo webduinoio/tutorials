a0b8124f013c11808c27eced1a73f19039b2430b
oxxo <oxxo.studio@gmail.com>
2016-09-20 11:40:17 +0800

    fixed browser's back event

--- app/js/tutorials-filter.js
+++ app/js/tutorials-filter.js
@@ -1,71 +1,51 @@
-$(function(){
+$(function() {
   var $window = $(window),
-  $div = $('.content>div'),
-  $btn = $('.filter div'),
-  $filter = $('.filter'),
-  $new = $('.filter-new'),
-  $info = $('.filter-info'),
-  $component = $('.filter-component'),
-  $sensor = $('.filter-sensor'),
-  $example = $('.filter-example'),
-  $btnAll = $('.btn-all'),
-  $btnNew = $('.btn-new'),
-  $btnInfo = $('.btn-info'),
-  $btnComponent = $('.btn-component'),
-  $btnSensor = $('.btn-sensor'),
-  $btnExample = $('.btn-example');
+    $div = $('.content>div'),
+    $btn = $('.filter a'),
+    $filter = $('.filter'),
+    $filterTag;
 
-  $window.scroll(function(){
-    if($window.scrollTop() > 0){
-      $filter.addClass('scroll'); 
-    }else{
+  hash();
+
+  function hash() {
+    $filterTag = location.href.split('#')[1];
+    if ($filterTag && $filterTag != '' && $filterTag != 'all') {
+      $btn.removeClass('click');
+      $('.btn-' + $filterTag).addClass('click');
+      $div.hide();
+      $('.filter-' + $filterTag).css({
+        'display': 'inline-block'
+      });
+    } else if ($filterTag == 'all') {
+      $btn.removeClass('click');
+      $('.btn-' + $filterTag).addClass('click');
+      $div.css({
+        'display': 'inline-block'
+      });
+    }
+    _lazyLoadRefresh();
+  }
+
+  $window.on('hashchange', function() {
+    hash();
+  });
+
+  $window.scroll(function() {
+    if ($window.scrollTop() > 0) {
+      $filter.addClass('scroll');
+    } else {
       $filter.removeClass('scroll');
     }
   });
 
-  $btn.on('click',function(){
-  	$btn.removeClass('click');
-  	$(this).addClass('click');
+  $btn.on('click', function() {
+    $btn.removeClass('click');
+    $(this).addClass('click');
   });
 
-  $btnAll.on('click',function(){
-  	$div.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  $btnNew.on('click',function(){
-  	$div.hide();
-  	$new.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  $btnInfo.on('click',function(){
-  	$div.hide();
-  	$info.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  $btnComponent.on('click',function(){
-  	$div.hide();
-  	$component.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  $btnSensor.on('click',function(){
-  	$div.hide();
-  	$sensor.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  $btnExample.on('click',function(){
-  	$div.hide();
-  	$example.css({'display':'inline-block'});
-  	_lazyLoadRefresh();
-  });
-
-  function _lazyLoadRefresh(){
-  	$window.scrollTop(1);
-  	$window.scrollTop(0);
+  function _lazyLoadRefresh() {
+    $window.scrollTop(1);
+    $window.scrollTop(0);
   }
 
 });
