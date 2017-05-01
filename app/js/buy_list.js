b0298f322d318b8b4d235c3cfe1a7f412cd2f08e
oxxo <oxxo.studio@gmail.com>
2017-05-01 20:12:13 +0800

    changed price

--- app/js/buy_list.js
+++ app/js/buy_list.js
@@ -6,14 +6,14 @@ $(function() {
       	var a = e.querySelector('a');
       	var i = e.querySelector('i');
         if (a.getAttribute('href') == 'buy/' + d.url) {
-          if (d.pricing != d.onSale) {
-            a.querySelector('.price').innerText = 'NT$ '+d.pricing;
-            a.querySelector('.onsale').innerText = 'NT$ '+d.onSale;
-          } else {
+          // if (d.pricing != d.onSale) {
+          //   a.querySelector('.price').innerText = 'NT$ '+d.pricing;
+          //   a.querySelector('.onsale').innerText = 'NT$ '+d.onSale;
+          // } else {
             a.querySelector('.price').innerText = 'NT$ '+d.pricing;
             a.querySelector('.price').style.textDecoration = 'none';
             a.querySelector('.price').style.fontSize = '18px';
-          }
+          // }
 	        if(d.new){
 	        	i.style.display = 'inline-block';
 	        }
