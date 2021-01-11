0973d86b6a032591dc1254611dba726ffd6b8660
oxxo <oxxo.studio@gmail.com>
2021-01-11 10:14:46 +0800

    to webduino.io

--- js/promote.js
+++ js/promote.js
@@ -1,1 +1,2 @@
+window.location.href = 'https://webduino.io';
 $(function(){$("a.activity").addClass("actived"),$("a").on("click",function(){$(this).attr("href");ga("send","event","Btn20170320","Click")})});
